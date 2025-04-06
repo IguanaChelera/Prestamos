260d4262cb06f26e6ae9eddc240714c24f78b863
iguano <ivanflash6@gmail.com>
2025-04-06 16:30:22 -0600

    Error logout

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,10 +1,15 @@
 @if(Auth::check())
+
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+</form>
+
 <nav class="sidebar nav flex-column pt-5">
     <a href="{{url('/catalogos/puestos')}}" class="nav-link">Puestos</a>
     <a href="{{url('/catalogos/empleados')}}" class="nav-link">Empleados</a>
     <a href="{{url('/movimientos/prestamos')}}" class="nav-link">Préstamos</a>
     <a href="{{url('/reportes/')}}" class="nav-link">Reportes</a>
-    <a href="{{url('/logout')}}" class="nav-link">Salir</a>
-    <a href = "#" onclick = "event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">Logout</a>
+    <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">Cerrar sesión</a>
 </nav>
+
 @endif
