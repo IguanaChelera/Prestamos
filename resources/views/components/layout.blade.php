b52ed12068ec27c753ead142c75d478b00efad5d
iguano <ivanflash6@gmail.com>
2025-04-06 16:17:48 -0600

    Estilos en RailWay

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,20 +3,20 @@
 
 <head>
     <!-- importar las librerías de bootstrap -->
-    <link rel="stylesheet" href="{{ URL::asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ secure_asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }}" />
 
     <!-- importar los archivos JavaScript de Bootstrap-->
-    <script src="{{ URL::asset('bootstrap-5.3.3-dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('bootstrap-5.3.3-dist/js/bootstrap.min.js') }}"></script>
 
     <!-- importar jQuery (necesario para DataTables) -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
     <!-- importar librerías de estilos y javascript de datatables -->
-    <link href="{{ URL::asset('DataTables/datatables.min.css') }}" rel="stylesheet" />
-    <script src="{{ URL::asset('DataTables/datatables.min.js') }}"></script>
+    <link href="{{ secure_asset('DataTables/datatables.min.css') }}" rel="stylesheet" />
+    <script src="{{ secure_asset('DataTables/datatables.min.js') }}"></script>
 
     <!-- estilos personalizados -->
-    <link href="{{ URL::asset('assets/style.css') }}" rel="stylesheet" />
+    <link href="{{ secure_asset('assets/style.css') }}" rel="stylesheet" />
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
