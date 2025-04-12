05d90bcde73cc4da024a3fc97738241a15444c56
iguano <ivanflash6@gmail.com>
2025-04-12 12:52:24 -0600

    Funciona

--- resources/views/movimientos/abonosGet.blade.php
+++ resources/views/movimientos/abonosGet.blade.php
@@ -28,42 +28,49 @@
         <a class = "btn btn-primary" href = '{{url("/prestamos/{$prestamo->id_prestamo}/abonos/agregar")}}'>Agregar</a>
     </div>
 </div>
-<table class="table" id="maintable">
+<table class = "table" id = "maintable">
 <thead>
     <tr>
-        <th scope="col">ID</th>
-        <th scope="col">NUM DE ABONO</th>
-        <th scope="col">FECHA</th>
-        <th scope="col">MONTO CAPITAL</th>
-        <th scope="col">MONTO INTERES</th>
-        <th scope="col">MONTO COBRADO</th>
-        <th scope="col">SALDO PENDIENTE</th>
+        <th scope = "col">ID</th>
+        <th scope = "col">NUM DE ABONO</th>
+        <th scope = "col">FECHA</th>
+        <th scope = "col">MONTO CAPITAL</th>
+        <th scope = "col">MONTO INTERES</th>
+        <th scope = "col">MONTO COBRADO</th>
+        <th scope = "col">SALDO PENDIENTE</th>
     </tr>
 </thead>
 <tbody>
 @foreach ($abonos as $abono)
     <tr>
         <td>{{$abono->id_abono}}</td>
-        <td class="text-center">{{$abono->num_abono}}</td>
-        <td>{{$abono->fecha}}</td>
-        <td class="text-end">{{number_format($abono->monto_capital, 2)}}</td>
-        <td class="text-end">{{number_format($abono->monto_interes, 2)}}</td>
-        <td class="text-end">{{number_format($abono->monto_cobrado, 2)}}</td>
-        <td class="text-end">{{number_format($abono->saldo_actual ?? $abono->saldo_pendiente, 2)}}</td>
+        <td class = "text-center">
+            {{$abono->num_abono}}
+        </td>
+        <td class = "text-end">
+            {{number_format($abono->monto_capital, 2)}}
+        </td>
+        <td class = "text-end">
+            {{number_format($abono->monto_interes, 2)}}
+        </td>
+        <td class = "text-end">
+            {{number_format($abono->monto_cobrado, 2)}}
+        </td>
+        <td class = "text-end">
+            {{number_format($abono->saldo_pendiente, 2)}}
+        </td>
     </tr>
 @endforeach
 </tbody>
 <tfoot>
     <tr>
-        <td colspan="3" class="text-end fw-bold">Total</td>
-        <td class="text-end">{{number_format($abonos->sum('monto_capital'), 2)}}</td>
-        <td class="text-end">{{number_format($abonos->sum('monto_interes'), 2)}}</td>
-        <td class="text-end">{{number_format($abonos->sum('monto_cobrado'), 2)}}</td>
-        <td></td>
+        <td colspan = "3" class = "text-end fw-bold">Total</td>
+        <td class = "text-end">{{number_format(array_sum(array_column($abonos, "monto_capital")), 2)}} </td>
+        <td class = "text-end">{{number_format(array_sum(array_column($abonos, "monto_interes")), 2)}}</td>
+        <td class = "text-end">{{number_format(array_sum(array_column($abonos, "monto_cobrado")),2 )}}</td>
     </tr>
 </tfoot>
 </table>
-
 <script>
     let table = document.getElementById("maintable")
     let datatable = new DataTable(table,{paging:true, searching:true})
